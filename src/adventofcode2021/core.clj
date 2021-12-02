67cece8a8b20c847b77c399794ad7010bf2634dc
Tycho Lamerigts <tycho.lamerigts@imc.com>
2021-12-02 18:42:12 +0100

    add day2-1

--- src/adventofcode2021/core.clj
+++ src/adventofcode2021/core.clj
@@ -7,18 +7,16 @@
   [name f]
   (map f (str/split-lines (slurp (io/resource name)))))
 
-(defn count-increased
-  [v]
-  (->> v
-       (partition 2 1)
-       (filter (partial apply <))
-       (count)))
+(def count-increased
+  (comp
+    (partition 2 1)
+    (filter (partial apply <))))
 
 (defn day1-1
   "--- Day 1: Sonar Sweep ---"
   [name]
   (let [v (inputs name #(Integer/parseInt %))]
-    (count-increased v)))
+    (into [] count-increased v)))
 
 (defn day1-2
   "--- Day 1 Part Two: Sonar Sweep ---"
@@ -28,3 +26,22 @@
          (partition 3 1)
          (map (partial reduce +))
          (count-increased))))
+
+(defn day2-parse-row
+  [s]
+  (map read-string (str/split (str ":" s) #"\s")))
+
+(defn nav
+  [[pos depth] [dir step]]
+  (condp = dir
+    :forward [(+ pos step) depth]
+    :down [pos (+ depth step)]
+    :up [pos (- depth step)]
+    ))
+
+(defn day2-1
+  "--- Day 2: Dive! ---"
+  [name]
+  (let [v (inputs name day2-parse-row)
+        loc (reduce nav [0 0] v)]
+    (reduce * loc)))
--- test/adventofcode2021/core_test.clj
+++ test/adventofcode2021/core_test.clj
@@ -9,3 +9,7 @@
 (deftest day1-2-test
   (is (= (day1-2 "1-example.txt") 5))
   (is (= (day1-2 "1.txt") 1702)))
+
+(deftest day2-1-test
+  (is (= (day2-1 "2-example.txt") 150))
+  (is (= (day2-1 "2.txt") 1250395)))
